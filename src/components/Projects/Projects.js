7e0fb64288787bb3d041a4a133b2d227e6742b65
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2023-06-23 10:04:01 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-// import leaf from "../../Assets/Projects/leaf.png";
-// import emotion from "../../Assets/Projects/emotion.png";
-// import editor from "../../Assets/Projects/codeEditor.png";
 import fitfusion from "../../Assets/Projects/fitfusion.png";
-// import suicide from "../../Assets/Projects/suicide.png";
+import whas from "../../Assets/Projects/whas.png";
 import discord from "../../Assets/Projects/discord.png";
+import modernBank from '../../Assets/Projects/modern-bank.png';
 
 function Projects() {
   return (
@@ -43,50 +41,29 @@ function Projects() {
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
+
+          <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={modernBank}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              title="Modern-Bank"
+              description="The Modern-Bank landing page website is a sleek and attractive React.js and Tailwind CSS project that showcases a modern UI/UX design, featuring a stunning hero section, billing section, testimonials section, clients section, and a stylish footer."
+              ghLink="https://github.com/HimeshKohad/Modern-Bank"
+              demoLink="https://modern-bank-landingpage.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={whas}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Wireless Home Automation System"
+              description='A wireless home automation project that incorporates multiple switching modes, enabling users to control devices through various methods. It includes Bluetooth connectivity for mobile control, traditional manual switches for convenient physical operation, and a binary counter for automated switching."
+              // ghLinkLink=""
+              demoLink="https://drive.google.com/file/d/1r2PgUFZLqO0DWOf66v8FmeA6RCt_PX-c/view?usp=drive_link"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
